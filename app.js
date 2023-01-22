c8d35b1b1c147b8c50f5f19bea183b9eb828bf8f
Daniil Yefimov <danyefimoff@gmail.com>
2023-01-22 16:13:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.post(
   '/signup',
   celebrate({
     body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
+      name: Joi.string().min(2).max(30),
       about: Joi.string().required().min(2).max(30),
       avatar: Joi.string().required().regex(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/),
       email: Joi.string().required().email(),
