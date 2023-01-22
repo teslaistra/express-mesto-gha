4f00db704aaf4d6d691dbb4885449ea381b642d6
Daniil Yefimov <danyefimoff@gmail.com>
2023-01-22 15:34:45 +0300

    user controller add

--- controllers/users.js
+++ controllers/users.js
@@ -40,3 +40,63 @@ module.exports.createUser = (req, res) => {
       res.status(500).send({ message: 'На сервере произошла ошибка' });
     });
 };
+
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
+};
+
+module.exports.getUser = (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (user) {
+        res.send({ data: user });
+      } else {
+        res.status(404).send({ message: 'Запрашиваемый пользователь не найден' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
+        res.status(400).send({ message: err.message });
+      }
+      res.status(500).send({ message: 'На сервере произошла ошибка' });
+    });
+};
+
+module.exports.updateUser = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+    .then((user) => {
+      if (user) {
+        res.send({ data: user });
+      } else {
+        res.status(404).send({ message: 'Запрашиваемый пользователь не найден' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
+        res.status(400).send({ message: err.message });
+      }
+      res.status(500).send({ message: 'На сервере произошла ошибка' });
+    });
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+    .then((user) => {
+      if (user) {
+        res.send({ data: user });
+      } else {
+        res.status(404).send({ message: 'Запрашиваемый пользователь не найден' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
+        res.status(400).send({ message: err.message });
+      }
+      res.status(500).send({ message: 'На сервере произошла ошибка' });
+    });
+};
+
