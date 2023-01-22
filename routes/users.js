4a2c3bddd9e1dbecec6d5100c7b6c1a07c47782a
Daniil Yefimov <danyefimoff@gmail.com>
2023-01-22 17:15:05 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const userRoutes = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 
 const {
-  getUsers, getUser, updateUser, updateAvatar, getMe
+  getUsers, getUser, updateUser, updateAvatar, getMe,
 } = require('../controllers/users');
 
 userRoutes.get('/users', getUsers);
