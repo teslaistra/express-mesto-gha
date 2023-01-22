dfb111d88faa6e558c65ded5124294e8cb375b6b
Daniil Yefimov <danyefimoff@gmail.com>
2023-01-22 16:45:03 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -9,7 +9,8 @@ module.exports.login = (req, res) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
-      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true }).end();
+      // res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true }).end();
+      res.send({ token });
     })
     .catch((err) => {
       res.status(401).send({ message: err.message });
