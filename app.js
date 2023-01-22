a0303eb6af134827edca8e8731c84486a67a40be
Daniil Yefimov <danyefimoff@gmail.com>
2023-01-22 16:57:46 +0300

    validation fix

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ app.post(
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
-      about: Joi.string().required().min(2).max(30),
-      avatar: Joi.string().required().regex(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().regex(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/),
       email: Joi.string().required().email(),
       password: Joi.string().required().min(8),
     }),
--- models/user.js
+++ models/user.js
@@ -5,21 +5,18 @@ const validator = require('validator');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: true,
     validate: {
       validator: (v) => validator.isURL(v),
       message: 'Неверный формат ссылки',
